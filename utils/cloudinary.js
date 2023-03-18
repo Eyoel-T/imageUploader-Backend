ef90b13581a875c2b7c78e2c56c9f6dc7b806dbc
eyoelT <eyoel@gmail.com>
2023-03-18 20:26:56 +0300

    final build

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -3,6 +3,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUDNAME,
   api_key: process.env.CLOUDAPIKEY,
   api_secret: process.env.CLOUDINARYSECRET,
-  secure: false,
+  secure: true,
 });
 module.exports = cloudinary;
